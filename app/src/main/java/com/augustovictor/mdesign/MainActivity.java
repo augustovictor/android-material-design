a979478e532c5039666244cf5b68ce04d2634fd1
Victor <victoraweb@gmail.com>
2016-05-26 16:33:03 -0300

    Standalone toolbar

--- app/src/main/java/com/augustovictor/mdesign/MainActivity.java
+++ app/src/main/java/com/augustovictor/mdesign/MainActivity.java
@@ -1,10 +1,8 @@
 package com.augustovictor.mdesign;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -18,55 +16,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Welcome!");
+        toolbar.setSubtitle("Folks! (standalone)");
 
-        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.menu_main);
 
-        getSupportActionBar().setTitle("Welcome!");
-        toolbar.setSubtitle("Folks!");
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            toolbar.setElevation(10f);
-        }
+                String msg = "";
 
-    }
+                switch (item.getItemId()) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+                    case R.id.menu_delete_opt:
+                        msg = "Delete option selected";
+                        break;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+                    case R.id.menu_search_opt:
+                        msg = "Search option selected";
+                        break;
 
-        String msg = "";
+                    case R.id.menu_edit_opt:
+                        msg = "Edit option selected";
+                        break;
 
-        switch (item.getItemId()) {
+                    case R.id.menu_settings_opt:
+                        msg = "Settings option selected";
+                        break;
 
-            case R.id.menu_delete_opt:
-                msg = "Delete option selected";
-                break;
+                    case R.id.menu_exit_opt:
+                        msg = "Exit option selected";
+                        break;
 
-            case R.id.menu_search_opt:
-                msg = "Search option selected";
-                break;
+                }
 
-            case R.id.menu_edit_opt:
-                msg = "Edit option selected";
-                break;
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
 
-            case R.id.menu_settings_opt:
-                msg = "Settings option selected";
-                break;
 
-            case R.id.menu_exit_opt:
-                msg = "Exit option selected";
-                break;
-
-        }
-
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        return super.onOptionsItemSelected(item);
+//        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            toolbar.setElevation(10f);
+//        }
 
     }
+
 }
