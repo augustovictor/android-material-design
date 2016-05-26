3377016475f5fa0c1f6476a2eed6ca0aab1cca54
Victor <victoraweb@gmail.com>
2016-05-26 16:25:16 -0300

    Adding icons and popup menu

--- app/src/main/java/com/augustovictor/mdesign/MainActivity.java
+++ app/src/main/java/com/augustovictor/mdesign/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,46 @@ public class MainActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             toolbar.setElevation(10f);
         }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String msg = "";
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_delete_opt:
+                msg = "Delete option selected";
+                break;
+
+            case R.id.menu_search_opt:
+                msg = "Search option selected";
+                break;
+
+            case R.id.menu_edit_opt:
+                msg = "Edit option selected";
+                break;
+
+            case R.id.menu_settings_opt:
+                msg = "Settings option selected";
+                break;
+
+            case R.id.menu_exit_opt:
+                msg = "Exit option selected";
+                break;
+
+        }
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
 
     }
 }
